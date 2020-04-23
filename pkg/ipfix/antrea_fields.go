aba658e021a91d720dd7453214b96fb02ee1ad37
Srikar Tati <stati@vmware.com>
2020-04-22 22:52:11 -0700

    Adding AddU8 function and extra antrea metrics for reverse counters

--- pkg/ipfix/antrea_fields.go
+++ pkg/ipfix/antrea_fields.go
@@ -7,4 +7,10 @@ const (
 	IPFIX_FIELD_Antrea_sourcePodName           = 177
 	IPFIX_FIELD_Antrea_destinationPodNamespace = 178
 	IPFIX_FIELD_Antrea_destinationPodName      = 179
+	IPFIX_FIELD_Antrea_reversePacketDeltaCount = 180
+	IPFIX_FIELD_Antrea_reverseByteDeltaCount   = 181
+	IPFIX_FIELD_Antrea_reversePacketTotalCount = 182
+	IPFIX_FIELD_Antrea_reverseByteTotalCount   = 183
+	IPFIX_FIELD_Antrea_sourceNodeName          = 184
+	IPFIX_FIELD_Antrea_destinationNodeName     = 185
 )
